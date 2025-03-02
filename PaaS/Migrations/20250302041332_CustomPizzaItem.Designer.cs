e1f2d60f5fc7e09ef904f9d72ffa1090d9af0dc8
eduzuniga <eduzsantillan@gmail.com>
2025-03-01 20:26:36 -0800

    feat : updating db context and adding migration to have custom pizza and more pre data

--- PaaS/Migrations/20250302041332_CustomPizzaItem.Designer.cs
+++ PaaS/Migrations/20250302041332_CustomPizzaItem.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PaaS.Data;
 
@@ -10,9 +11,11 @@ using PaaS.Data;
 namespace PaaS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250302041332_CustomPizzaItem")]
+    partial class CustomPizzaItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
