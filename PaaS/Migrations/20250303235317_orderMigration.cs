c0d9c4ffcce1a042a22dcfa92fee7b88899ce7b0
psyuan77@hotmail.com <156147051+psyuan77@users.noreply.github.com>
2025-03-03 15:57:45 -0800

    adding migrations

---
+++ PaaS/Migrations/20250303235317_orderMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaaS.Migrations
+{
+    /// <inheritdoc />
+    public partial class orderMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Phone",
+                table: "ContactInfo",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "ContactInfo");
+        }
+    }
+}
